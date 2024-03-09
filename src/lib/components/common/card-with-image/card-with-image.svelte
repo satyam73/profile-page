e1973c054da1e29e4dc364956d0059a19e1f39a1
Satyam <bajpai1973satyam@gmail.com>
2024-03-09 18:55:45 +0530

    feat: add card with image component

---
+++ src/lib/components/common/card-with-image/card-with-image.svelte
@@ -0,0 +1,11 @@
+<script lang='ts'>
+  export let image: string;
+  export let text: string;
+</script>
+
+<div class="w-[344px] border rounded-xl overflow-hidden">
+  <img class="object-cover w-full h-[136px]" src={image} alt="">
+  <div class="h-[90px] flex items-center px-3 text-[#141618] text-sm font-normal">
+    {text}
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
+	import CardWithImage from '$lib/components/common/card-with-image/card-with-image.svelte';
 	import ProfileHeader from '$lib/components/profile-header/profile-header.svelte';
+  
 </script>
 
 <div>
 	<ProfileHeader />
+
+  <CardWithImage image={'/steve-jobs-bg.svg'} text={'No amount of technology can convert a bad story into a good story.'}/>
 </div>
