bec03356df11d423e33d0c1f8e92ae316993bf41
Satyam <bajpai1973satyam@gmail.com>
2024-03-10 13:31:56 +0530

    feat: add circle for timeline component

---
+++ src/lib/components/common/timeline/circle.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { cn } from '$lib/utils';
+
+	export let isBigCircle: boolean;
+	export let styles: string = '';
+</script>
+
+{#if isBigCircle}
+	<span class={cn(`block h-7 w-7 rounded-full border-[9px] border-[#0066FF] ${styles}`)}></span>
+{:else}
+	<span class={cn(`block h-4 w-4 rounded-full border-[3px] border-[#0066FF] ${styles}`)}></span>
+{/if}
