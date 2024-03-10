f3329a85a60fa8cf6ed7d5982e9e12b21dc783d2
Satyam <bajpai1973satyam@gmail.com>
2024-03-10 17:24:16 +0530

    styles: add height to card image component

--- src/lib/components/common/card-with-image/card-with-image.svelte
+++ src/lib/components/common/card-with-image/card-with-image.svelte
@@ -3,7 +3,7 @@
   export let text: string;
 </script>
 
-<div class="w-[344px] border rounded-xl overflow-hidden">
+<div class="w-[344px] h-[226px] border rounded-xl overflow-hidden">
   <img class="object-cover w-full h-[136px]" src={image} alt="">
   <div class="h-[90px] flex items-center px-3 text-[#141618] text-sm font-normal">
     {text}
