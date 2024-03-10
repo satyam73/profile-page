e14c2da5bfbdb79b7414f7acf76aaa39fcb827c0
Satyam <bajpai1973satyam@gmail.com>
2024-03-10 17:53:30 +0530

    refactor: card with image component

--- src/lib/components/common/card-with-image/card-with-image.svelte
+++ src/lib/components/common/card-with-image/card-with-image.svelte
@@ -1,11 +1,16 @@
 <script lang='ts'>
   export let image: string;
   export let text: string;
+  export let link: string = 'https://google.com';
 </script>
 
-<div class="w-[344px] h-[226px] border rounded-xl overflow-hidden">
-  <img class="object-cover w-full h-[136px]" src={image} alt="">
-  <div class="h-[90px] flex items-center px-3 text-[#141618] text-sm font-normal">
-    {text}
+<a href={link} target="_blank">
+  <div class="w-[344px] h-[226px] border rounded-xl overflow-hidden">
+    <img class="object-cover w-full h-[136px]" src={image} alt="">
+    <div class="h-[90px] flex items-center px-3 text-[#141618] text-sm font-normal">
+      <div class="h-[41px] line-clamp-2">
+        {text}
+      </div>
+    </div>
   </div>
-</div>
+</a>
