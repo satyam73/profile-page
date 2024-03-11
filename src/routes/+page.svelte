e5a5953e087ca789d942a39d22aa98aeac8014f6
Satyam <bajpai1973satyam@gmail.com>
2024-03-11 15:11:52 +0530

    feat: add title and favicon to page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,12 +3,10 @@
 	import ProfileHeader from '$lib/components/profile-header/profile-header.svelte';
 </script>
 
+<svelte:head>
+	<title>Duggup | Profile</title>
+</svelte:head>
 <div class="flex flex-col gap-4">
 	<ProfileHeader />
-	<!-- <div class="h-[100vh] bg-slate-400" >
-		color
-	</div> -->
-	<!-- <div class="w-full overflow-auto "> -->
 	<Timeline />
-	<!-- </div> -->
 </div>
